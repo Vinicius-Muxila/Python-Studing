8019493e6a7b240a8caa2c18243156e7d2999a9d
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-06-30 21:53:12 -0300

    testes utilizando fastApi

---
+++ fastApi_studing.py
@@ -0,0 +1,17 @@
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("vinícius", "henrique"))
+
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old: " + str(age)
+    return name_with_age
