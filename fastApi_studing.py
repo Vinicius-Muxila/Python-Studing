224dd4fb39632bdb8fb47c256413dc879a71521e
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-07-16 15:03:44 -0300

    Teste FastApi

--- fastApi_studing.py
+++ fastApi_studing.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def get_full_name(first_name, last_name):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
@@ -15,3 +17,11 @@ print(get_full_name("john", "doe"))
 def get_name_with_age(name: str, age: int):
     name_with_age = name + " is this old: " + str(age)
     return name_with_age
+
+
+
+def say_hi(name: Optional[str] = None):
+    if name is not None:
+        print(f"Hey {name}!")
+    else:
+        print("Hello World")
